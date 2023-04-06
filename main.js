23c71806202b5fa72adc5005f4bfd238e92f9b9b
Diogo Sela <diogosella@outlook.com>
2023-04-06 02:38:23 -0300

    Homework commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form-list');
+const nomes = [];
+const numeros = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adcionalinha();
+    adcionaTabela();
+});
+
+    function adcionalinha() {
+    const inputNome = document.getElementById('nome');
+    const inputNumero = document.getElementById('numero');
+
+    if (nomes.includes(inputNome.value)) {
+        alert(`O contato ja foi inserido!`)
+    } else {
+    nomes.push(inputNome.value);
+    numeros.push(inputNome.value);
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputNumero.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+    }
+
+    inputNome.value = '';
+    inputNumero.value = '';
+}
+
+    function adcionaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+    }
+
